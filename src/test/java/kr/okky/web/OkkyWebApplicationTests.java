240b99c7a8df4ae072dd0c872e63712ab41e4233
Mambo <kdevkr@gmail.com>
2023-03-29 22:09:09 +0900

    Generate Spring Boot 3.0 project

---
+++ src/test/java/kr/okky/web/OkkyWebApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.okky.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OkkyWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
